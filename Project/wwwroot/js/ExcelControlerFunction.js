1308745c6b0609594ddd37bf6faeedcadbc76aab
Kevin Jose Mena Gomez <80128262+Kevinzen88@users.noreply.github.com>
2023-12-04 10:48:28 -0600

    Organizacion de Datos
    Eliminación de datos basura, simplicación y organización de funciones

---
+++ Project/wwwroot/js/ExcelControlerFunction.js
@@ -0,0 +1,6 @@
+
+
+/*Funciones js para el apartado de excel's y Modal*/
+document.write('<script src="~/js/excel-viewer.js?v=2"></script>');
+document.write('<script src="~/js/loadPartialViews.js?v=2"></script>');
+document.write('<script src="~/js/excel-datos.js?v=2"></script>');
